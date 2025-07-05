bb75a2c4484a6e218a194c0773c27f027c25ccd7
Bhargav Makwana <bhargavmakwana260804@gmail.com>
2025-07-05 16:30:22 +0530

    step-2:Test case for single digit and multiplt digit.

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -4,3 +4,16 @@ const add = require('../src/calculator');
 test('returns 0 for an empty string', () => {
   expect(add("")).toBe(0);
 });
+
+// Test case for return sum of single digit and multiple digit in string.
+test('returns 0 for an empty string', () => {
+  expect(add("0")).toBe(0);
+});
+
+test('returns digit for single digit in string', () => {
+  expect(add("5")).toBe(5);
+});
+
+test('returns sum for multiple digit in string', () => {
+  expect(add("5,4,3")).toBe(12);
+});
